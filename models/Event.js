ef3bdd963f5fce5fe639d4da8e1aae6d8d9b20a9
augusticor <rojascruzoscar@gmail.com>
2025-01-14 17:07:58 -0500

    Update dependencies and Event schema for timestamp

--- models/Event.js
+++ models/Event.js
@@ -1,27 +1,32 @@
 import { model, Schema } from 'mongoose';
 
-const EventSchema = Schema({
-  title: {
-    type: String,
-    required: true,
+const EventSchema = Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    notes: {
+      type: String,
+    },
+    start: {
+      type: Date,
+      required: true,
+    },
+    end: {
+      type: Date,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
-  notes: {
-    type: String,
+  {
+    timestamps: true,
   },
-  start: {
-    type: Date,
-    required: true,
-  },
-  end: {
-    type: Date,
-    required: true,
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-});
+);
 
 // Schema overwrite, remove __v and rename _id to id
 EventSchema.method('toJSON', function () {
